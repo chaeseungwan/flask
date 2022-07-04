cbcf2cee47a0615446fa6c836e046a6f82464f46
chaeseungwan <industry95@naver.com>
2022-07-04 23:39:18 +0900

    git add

--- flask_app.py
+++ flask_app.py
@@ -42,7 +42,7 @@ def report():
     return render_template('graph.html', word=word, date=date, count=count, google_date=google_date, google_count=google_count)
 
 
-if __name__ == '__main__':
-    app.run()
+#if __name__ == '__main__':
+ #   app.run()
 
-#app.run(host="127.0.0.1")
+app.run(host="127.0.0.1")
